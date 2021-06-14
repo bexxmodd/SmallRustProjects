2dadf50a2489ade4a4b440c7e9a80c614fd79afe
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-06-14 12:21:08 -0400

    avg sqr and decimal methods

--- fractions/src/lib.rs
+++ fractions/src/lib.rs
@@ -44,7 +44,14 @@ pub struct Fraction {
 
 impl Fraction {
     pub fn new(numerator: i32, denominator: i32) -> Self {
-        Fraction { numerator, denominator, }
+        if denominator < 0 {
+            Fraction {
+                numerator: numerator * -1,
+                denominator: denominator * -1,
+            }
+        } else {
+            Fraction { numerator, denominator, }
+        }
     }
 
     pub fn reduce_form(&mut self) {
@@ -59,21 +66,70 @@ impl Fraction {
         self.denominator /= gcf;
     }
 
-    pub fn add(&self, other: Fraction) -> Fraction {
+    pub fn add(&self, other: &Fraction) -> Fraction {
         let lcm = i32::least_common_multiple(
             self.denominator, other.denominator);
         
         let frac_multiple = lcm / self.denominator;
         let other_mutplie = lcm / other.denominator;
 
-        let numerator = frac_multiple * self.numerator +
-                            other_mutplie * other.numerator;
+        let numerator = frac_multiple * self.numerator
+                            + other_mutplie * other.numerator;
         
         let mut f = Fraction::new(numerator, lcm);
         f.reduce_form();
         f
     }
+
+    pub fn sub(&self, other: &Fraction) -> Fraction {
+        let lcm = i32::least_common_multiple(
+            self.denominator, other.denominator);
+        
+        let fraction_multiple = lcm / self.denominator;
+        let other_multiple = lcm / other.denominator;
+
+        let numer = fraction_multiple * self.numerator 
+                        - other_multiple * other.numerator;
+        
+        let mut f = Fraction::new(numer, lcm);
+        f.reduce_form();
+        f
+    }
+
+    pub fn mul(&self, other: &Fraction) -> Fraction {
+        let new_numer = self.numerator * other.numerator;
+        let new_denom = self.denominator * other.denominator;
+
+        let mut f = Fraction::new(new_numer, new_denom);
+        f.reduce_form();
+        f
+    }
+
+    pub fn div(&self, other: &Fraction) -> Fraction {
+        let mut f = self.mul(&Fraction::new(
+            other.denominator, other.numerator));
+        f.reduce_form();
+        f
+    }
+
+    pub fn to_decimal(&self) -> f64 {
+        self.numerator as f64 / self.denominator as f64
+    }
+
+    pub fn sqr(&mut self) {
+        self.numerator *= self.numerator;
+        self.denominator *= self.denominator;
+        self.reduce_form();
+    }
+
+    pub fn avg(&self, other: &Fraction) -> Fraction {
+        let frac_sum = self.add(other);
+        let mut f = frac_sum.mul(&Fraction::new(1, 2));
+        f.reduce_form();
+        f
+    }
 }
+
 
 
 
@@ -121,9 +177,51 @@ mod tests {
     }
 
     #[test]
-    fn test_addition() {
+    fn test_add() {
         let f1 = Fraction::new(3, 8);
         let f2 = Fraction::new(1, 8);
-        assert_eq!(Fraction::new(1, 2), f1.add(f2));
+        assert_eq!(Fraction::new(1, 2), f1.add(&f2));
+    }
+
+    #[test]
+    fn test_sub() {
+        let f1 = Fraction::new(3, 8);
+        let f2 = Fraction::new(1, 8);
+        assert_eq!(Fraction::new(1, 4), f1.sub(&f2));
+    }
+
+    #[test]
+    fn test_mul() {
+        let f1 = Fraction::new(3, 8);
+        let f2 = Fraction::new(2, 8);
+        assert_eq!(Fraction::new(3, 32), f1.mul(&f2));
+    }
+
+    #[test]
+    fn test_div() {
+        let f1 = Fraction::new(3, 8);
+        let f2 = Fraction::new(2, 4);
+        assert_eq!(Fraction::new(3, 4), f1.div(&f2));
+    }
+
+    #[test]
+    fn test_decimal() {
+        let f1 = Fraction::new(3, 8).to_decimal();
+        assert_eq!(0.375f64, f1);
+    }
+
+    #[test]
+    fn test_sqr() {
+        let mut f1 = Fraction::new(3, 4);
+        f1.sqr();
+        assert_eq!(Fraction::new(9, 16), f1);
+    }
+
+    #[test]
+    fn test_avg() {
+
+        let f1 = &Fraction::new(3, 4)
+                                        .avg(&Fraction::new(1, 2));
+        assert_eq!(Fraction::new(5, 8), *f1);
     }
 }
