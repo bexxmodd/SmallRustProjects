69f537fee0e4076d2924c3119a069842f488fd13
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-07-08 22:16:21 -0400

    threads test

---
+++ threads/target/debug/deps/threads-aacfb46c1c8f865a.d
@@ -0,0 +1,5 @@
+/home/bexx/Projects/SmallRustProjects/threads/target/debug/deps/threads-aacfb46c1c8f865a: src/main.rs
+
+/home/bexx/Projects/SmallRustProjects/threads/target/debug/deps/threads-aacfb46c1c8f865a.d: src/main.rs
+
+src/main.rs:
---
+++ threads/target/debug/threads.d
@@ -0,0 +1,1 @@
+/home/bexx/Projects/SmallRustProjects/threads/target/debug/threads: /home/bexx/Projects/SmallRustProjects/threads/src/main.rs
