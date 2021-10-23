ecb70010249715f0e140d66b3dbd3a629cc832dd
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-10-23 14:15:17 -0400

    refCell rc

--- structs/src/lib.rs
+++ structs/src/lib.rs
@@ -1,3 +1,33 @@
+use std::borrow::{Borrow, BorrowMut};
+use std::rc::Rc;
+
+struct Node {
+    tag: String,
+    children: Vec<Rc<Node>>,
+}
+
+impl Node {
+    fn new(tag :&str) -> Self {
+        Node {
+            tag: tag.to_string(),
+                 children: vec![],
+        }
+    }
+
+    fn append_to(self: Rc<Self>, parent: &mut Node) {
+        parent.children.push(self);
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Cup {
+    capacity: u32,
+}
+
+impl Cup {
+    const EMPTY: Cup = Cup {  capacity: 0, };
+}
+
 struct Broom {
     name: String,
     height: u32,
@@ -18,20 +48,32 @@ impl Broom {
     }
 }
 
+struct Extrema<'a> {
+    greatest: &'a i32,
+    least: &'a i32,
+}
+
+pub struct Queue<T> {
+    older: Vec<T>,
+    younger: Vec<T>,
+}
+
 #[derive(Copy, Clone)]
 struct Position(f32, f32, f32);
 
-pub struct Queue {
-    older: Vec<char>,
-    younger: Vec<char>,
-}
-
-impl Queue {
-    pub fn push(&mut self, c: char) {
-        self.younger.push(c);
+impl<T> Queue<T> {
+    pub fn new() -> Self {
+        Queue { 
+            older: Vec::new(),
+            younger: Vec::new(),
+        }
     }
 
-    pub fn pop(&mut self) -> Option<char> {
+    pub fn push(&mut self, t: T) {
+        self.younger.push(t);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
         if self.older.is_empty() {
             if self.younger.is_empty() {
                 return None;
@@ -47,6 +89,22 @@ impl Queue {
 
 #[derive(Copy, Clone)]
 enum BroomIntent { FetchWater, DumpWater }
+
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::Write;
+
+pub struct SpiderRobot {
+    log_file: RefCell<File>,
+}
+
+impl SpiderRobot {
+    /// write a line to the log File
+    pub fn log(&self, message: &str) {
+        let mut file = self.log_file.borrow_mut();
+        writeln!(file, "{}", message).unwrap();
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -74,7 +132,7 @@ mod tests {
 
     #[test]
     fn queue_works() {
-        let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+        let mut q = Queue::new(); 
 
         q.push('0');
         q.push('1');
@@ -84,5 +142,35 @@ mod tests {
         assert_eq!(q.pop(), Some('1'));
         assert_eq!(q.pop(), Some('∞'));
         assert_eq!(q.pop(), None);
+    }
+
+    #[test]
+    fn test_nodes() {
+        let mut parent = Node::new("root");
+        let shared_node = Rc::new(Node::new("first"));
+        shared_node.append_to(&mut parent);
+
+        let owned = Node::new("owned directly");
+        Rc::new(owned).append_to(&mut parent);
+        assert_eq!(parent.children.len(), 2);
+    }
+
+    #[test]
+    fn test_find_extrema() {
+        fn find_extrema(slice: &[i32]) -> Extrema {
+            let mut greatest = &slice[0];
+            let mut least = &slice[0];
+
+            for i in 1..slice.len() {
+                if slice[i] < *least    { least    = &slice[i]; }
+                if slice[i] > *greatest { greatest = &slice[i]; }
+            }
+            Extrema { greatest, least }
+        }
+
+        let a = [0, -3, 0, 15, 48];
+        let e = find_extrema(&a);
+        assert_eq!(*e.least, -3);
+        assert_eq!(*e.greatest, 48);
     }
 }
