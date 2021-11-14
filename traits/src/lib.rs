217cf000c5b07d6bd997d8ca9a0da15f70c7e528
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-11-14 17:10:43 -0500

    operator overloading

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,26 +1,82 @@
 use std::fmt;
+use std::iter;
+use std::vec::IntoIter;
 
-struct Wallet<T> {
-    brand: String,
-    contents: Vec<T>,
+struct Triangle {
+    a: u32,
+    b: u32,
+    c: u32,
 }
 
-impl<T> fmt::Display for Wallet<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Card {} has {} cards stored",
-            self.brand, self.contents.len())
+struct Square {
+    a: u32,
+}
+
+
+trait Shape {
+    const NAME: &'static str = "Shape ::> ";
+    fn new() -> Self;
+    fn area(&self) -> f64;
+    fn shape_name(&self) -> String;
+}
+
+impl Shape for Triangle {
+    fn new() -> Self {
+        Triangle { a: 10, b: 10, c: 15, }
+    }
+
+    fn area(&self) -> f64 {
+        let h = 8f64;
+        0.5 * (self.c as f64 * h)
+    }
+
+    fn shape_name(&self) -> String {
+        let mut res = String::new();
+        res.push_str(Self::NAME);
+        res.push_str("Triangle");
+        res
+    }
+
+} 
+
+fn print_shape(shape: impl Shape) {
+    println!(">>> Area: {}", shape.area());
+}
+
+use std::ops::Add;
+
+#[derive(Clone, Copy, Debug)]
+struct Complex<T> {
+    re: T,
+    im: T,
+}
+
+impl<L, R> Add<Complex<R>> for Complex<L>
+where
+    L: Add<R>,
+{
+    type Output = Complex<L::Output>;
+
+    fn add(self, rhs: Complex<R>) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im,
+        }
     }
 }
 
-trait Print {
-    fn print(&self);
-}
+use std::ops::Neg;
 
-impl<C: Iterator + fmt::Display + Debug> Print for C {
-    fn print(&self) {
-        self.into_iter().for_each(|i| {
-            println!("{:?}", i);
-        });
+impl<T> Neg for Complex<T>
+where
+    T: Neg<Output = T>,
+{
+    type Output = Complex<T>;
+    fn neg(self) -> Complex<T> {
+        Complex {
+            re: -self.re,
+            im: -self.im,
+        }
     }
 }
 
@@ -30,11 +86,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let w: Wallet<i32> = Wallet {
-            brand: "Calvin".to_string(),
-            contents: vec![],
-        };
-        assert_eq!(w.to_string(), "Wallet Calvin has 0 cards in it");
+        let s = Triangle::new();
+        let name = s.shape_name();
+        assert_eq!("Shape ::> Triangle".to_string(), name);
     }
 }
 
